704b09735bee00ba0fff47822b7e4183c3fe1195
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-18 23:07:23 +0300

    BaseLayout component created

---
+++ src/components/BaseLayout.tsx
@@ -0,0 +1,46 @@
+import React, {ReactNode} from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  View,
+  ImageSourcePropType,
+} from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+
+interface BaseLayoutProps {
+  children: ReactNode;
+  image: ImageSourcePropType; // Dışarıdan alınacak görsel
+}
+
+const BaseLayout: React.FC<BaseLayoutProps> = ({children, image}) => {
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {/* Üstteki Image */}
+      <Image source={image} style={styles.image} resizeMode="cover" />
+      {/* Sayfa İçeriği */}
+      <View style={styles.content}>{children}</View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  image: {
+    width: wp(100),
+    height: hp(20),
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: wp(5),
+    marginTop: -hp(5),
+  },
+});
+
+export default BaseLayout;
