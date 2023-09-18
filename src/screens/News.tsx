55a46bd17d89c7c5ee6234b7f60ac643cd040307
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-18 23:06:04 +0300

    Created news screen

---
+++ src/screens/News.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BaseLayout from '../components/BaseLayout';
+
+const News = () => {
+  return (
+    <View>
+      <Text>dsdsd</Text>
+    </View>
+  );
+};
+
+export default News;
+
+const styles = StyleSheet.create({});
