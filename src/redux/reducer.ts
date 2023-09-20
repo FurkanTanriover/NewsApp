3d1e406633613c64dbfde34ff0624ccfb60bc913
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-20 13:35:36 +0300

    Updated redux reducer and constants

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,32 +1,11 @@
-import {GET_POST_DATA, LOGIN_FAILURE, LOGIN_SUCCESS} from '../utils/constants';
+import {GET_POST_DATA} from '../utils/constants';
 
 const initialState = {
-  token: null,
-  error: null,
-  data: null,
-  insightsPosts: [],
   posts: [],
-  isAuth: false,
-  isRegisterSuccess: false,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        token: action.payload,
-        error: null,
-        isAuth: action.payload,
-      };
-
-    case LOGIN_FAILURE:
-      return {
-        ...state,
-        token: null,
-        error: action.payload || 'Bir hata oluştu',
-      };
-
     case GET_POST_DATA:
       return {
         ...state,
