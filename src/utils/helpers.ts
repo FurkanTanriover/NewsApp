2445eeec84ce490795f786b2d196f633c0dec3dd
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-20 13:38:59 +0300

    Create daysAgoFromDate helper

---
+++ src/utils/helpers.ts
@@ -0,0 +1,34 @@
+import {Linking} from 'react-native';
+
+function daysAgoFromDate(dateString) {
+  // Verilen tarihi bir Date nesnesine çevirin
+  const date = new Date(dateString);
+
+  // Bugünkü tarihi alın
+  const today = new Date();
+
+  // İki tarih arasındaki milisaniye farkını hesaplayın
+  const timeDifference = today - date;
+
+  // Milisaniyeyi güne ve saate çevirin
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+  const daysDifference = Math.floor(timeDifference / millisecondsPerDay);
+  const millisecondsPerHour = 60 * 60 * 1000;
+  const hoursDifference = Math.floor(
+    (timeDifference % millisecondsPerDay) / millisecondsPerHour,
+  );
+
+  if (daysDifference > 0) {
+    if (hoursDifference > 0) {
+      return `${daysDifference} gün ${hoursDifference} saat önce`;
+    } else {
+      return `${daysDifference} gün önce`;
+    }
+  } else if (hoursDifference > 0) {
+    return `${hoursDifference} saat önce`;
+  } else {
+    return 'Şimdi';
+  }
+}
+
+export {daysAgoFromDate};
