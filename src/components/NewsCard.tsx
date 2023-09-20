323341b4172e248d2bd72df621e1dfe91dd9fa6b
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-09-20 13:38:15 +0300

    Backend entegration with NewsCard component

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  ImageSourcePropType,
-  TouchableOpacity,
-} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
-
+import {daysAgoFromDate} from '../utils/helpers';
+import WebPage from '../screens/WebPage';
+import {useNavigation} from '@react-navigation/native';
 interface NewsCardProps {
-  category: string;
-  description: string;
-  author: string;
-  daysAgo: number;
-  imageUrl: ImageSourcePropType;
+  data: [];
   cardHeight?: number;
   cardWidth?: number;
   imageHeight?: number;
@@ -25,54 +16,61 @@ interface NewsCardProps {
 }
 
 const NewsCard: React.FC<NewsCardProps> = ({
-  category,
-  description,
-  author,
-  daysAgo,
-  imageUrl,
+  data,
   cardHeight = hp(22), // Ekran yüksekliğinin %20'si
   cardWidth = wp(90), // Ekran genişliğinin %90'ı
 }) => {
+  const navigation = useNavigation();
+  const daysAgo = daysAgoFromDate(data.published_at);
+  const openWebPage = () => {
+    navigation.navigate('WebPage', {url: data.url});
+  };
   return (
-    <View style={[styles.container, {height: cardHeight, width: cardWidth}]}>
-      {/* left container */}
-      <View style={styles.leftContainer}>
-        <View style={styles.categoryTextContainer}>
-          <Text>{category}</Text>
+    <TouchableOpacity onPress={() => openWebPage()}>
+      <View style={[styles.container, {height: cardHeight, width: cardWidth}]}>
+        <View style={styles.titleSection}>
+          <Text className="uppercase text-gray-500 text-xs font-bold">
+            {data.categories[0].title}
+          </Text>
         </View>
-        <View style={styles.descriptionTextContainer}>
-          <Text>{description}</Text>
+        <View style={styles.descAndImageSection}>
+          <View style={styles.descriptionSection}>
+            <Text className="text-black text-base font-bold">{data.title}</Text>
+          </View>
+          <View style={styles.imageSection}>
+            <Image
+              source={data.thumbnails.full}
+              style={{width: '100%', height: '100%'}}
+              resizeMode="contain"
+            />
+          </View>
         </View>
-        <View style={styles.authorDaysAgoContainer}>
-          <Text>{author}</Text>
-          <Text> -- </Text>
-          <Text>{daysAgo} days ago</Text>
+        <View style={styles.authorAndCollectionSection}>
+          <View style={styles.authorSection}>
+            <Text className="text-gray-700 font-bold">
+              {data.author.full_name}
+            </Text>
+            <Text> 〰 </Text>
+            <Text className="text-gray-700 font-normal">{daysAgo}</Text>
+          </View>
+          <View style={styles.collectionSection}>
+            <TouchableOpacity onPress={() => console.log('Koleksiyon')}>
+              <Image
+                className="w-6 h-6"
+                source={require('../assets/collection.png')}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-      {/* right container */}
-      <View style={styles.rightContainer}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={imageUrl}
-            style={{width: wp(28)}}
-            resizeMode="contain"
-          />
-        </View>
-        <View style={styles.addCollectionContainer}>
-          <Image
-            source={require('./../assets/collection.png')}
-            style={{width: wp(5)}}
-            resizeMode="contain"
-          />
-        </View>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     backgroundColor: 'white',
     borderRadius: 5,
     padding: wp(4),
@@ -82,47 +80,40 @@ const styles = StyleSheet.create({
     shadowColor: 'gray',
     shadowOpacity: 0.2,
   },
-  leftContainer: {
-    width: '55%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  rightContainer: {
-    width: '45%',
-    height: '100%',
-    paddingLeft: '11%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  categoryTextContainer: {
+  titleSection: {
     width: '100%',
-    height: '20%',
+    height: '15%',
   },
-  descriptionTextContainer: {
+  descAndImageSection: {
     width: '100%',
-    height: '60%',
-  },
-  authorDaysAgoContainer: {
+    height: '70%',
     flexDirection: 'row',
-    width: '100%',
-    height: '20%',
   },
-  imageContainer: {
-    marginTop: hp(4),
-    width: '100%',
-    height: '60%',
+  descriptionSection: {
+    width: '60%',
+    marginRight: '5%',
+    height: '100%',
+  },
+  imageSection: {
+    width: '35%',
+    height: '100%',
+  },
+  authorAndCollectionSection: {
+    height: '15%',
+    flexDirection: 'row',
+  },
+  authorSection: {
+    width: '70%',
+    height: '100%',
     display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  addCollectionContainer: {
-    width: '100%',
-    height: '20%',
-    marginTop: '10%',
+  collectionSection: {
+    width: '30%',
+    height: '100%',
     display: 'flex',
     alignItems: 'flex-end',
-    justifyContent: 'center',
   },
 });
 
